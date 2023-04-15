73ab90e51e31d6d7413998f564c1c0bc4754975c
Shivani-Dhawadkar <120394193+Shivani-Dhawadkar@users.noreply.github.com>
2023-04-15 20:15:02 +0530

    1st emit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NewComponent } from './new/new.component';
+import { productComponent } from './product/product.component';
+import { shivanicomponent } from './shivani/shivani.component';
+import { productsComponent  } from './products/products.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewComponent,
+    productComponent,
+    productsComponent,
+    shivanicomponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
